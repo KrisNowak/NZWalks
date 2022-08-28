8dd0153fe0fd2383628d3103cc728e0feba32a0e
KrisNowak <ojcieckrzych@gmail.com>
2022-08-28 16:39:33 +0200

    End Of Section 3

---
+++ NZWalks/NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,59 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.Domain;
+using NZWalks.API.Repositories;
+
+namespace NZWalks.API.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class RegionsController : Controller
+    {
+        private readonly IRegionRepository regionRepository;
+        private readonly IMapper mapper;
+
+        public RegionsController(IRegionRepository regionRepository, IMapper mapper)
+        {
+            this.regionRepository = regionRepository;
+            this.mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllRegions()
+        {
+            //var regions = new List<Region>()
+            //{
+            //    new Region
+            //    {
+            //        Id = Guid.NewGuid(),
+            //        Name = "Wellington",
+            //        Code = "WLG",
+            //        Area = 227755,
+            //        Lat = -1.8822,
+            //        Long = 299.88,
+            //        Population = 500000
+            //    },
+            //    new Region
+            //    {
+            //        Id = Guid.NewGuid(),
+            //        Name = "Auckland",
+            //        Code = "AUCK",
+            //        Area = 227755,
+            //        Lat = -1.8822,
+            //        Long = 299.88,
+            //        Population = 500000
+            //    }
+            //};
+
+            var regions = await regionRepository.GetAllAsync();
+
+            //var regionsDTO = regions.ToList().Select(region => new Models.DTO.Region() { Id = region.Id, Area = region.Area, Code = region.Code, Lat = region.Lat, Long = region.Long, Population = region.Population, Name = region.Name });
+            var regionsDTO = mapper.Map<List<Models.DTO.Region>>(regions);
+
+            // return DTO Regions
+            
+
+            return Ok(regionsDTO);
+        }
+    }
+}
